1064d031e989242fd302dbd9249fbec4b25177b8
Tingluo Huang <tingluohuang@github.com>
2026-05-13 20:01:53 +0000

    Add github/codeql-action to cache.

---
+++ script/generated/github_codeql-action.sh
@@ -0,0 +1,9 @@
+mkdir github_codeql-action
+pushd github_codeql-action
+curl -s -S -L -o '68bde559dea0fdcac2102bfdf6230c5f70eb485e.tar.gz' 'https://api.github.com/repos/github/codeql-action/tarball/68bde559dea0fdcac2102bfdf6230c5f70eb485e'
+curl -s -S -L -o '68bde559dea0fdcac2102bfdf6230c5f70eb485e.zip' 'https://api.github.com/repos/github/codeql-action/zipball/68bde559dea0fdcac2102bfdf6230c5f70eb485e'
+curl -s -S -L -o 'e46ed2cbd01164d986452f91f178727624ae40d7.tar.gz' 'https://api.github.com/repos/github/codeql-action/tarball/e46ed2cbd01164d986452f91f178727624ae40d7'
+curl -s -S -L -o 'e46ed2cbd01164d986452f91f178727624ae40d7.zip' 'https://api.github.com/repos/github/codeql-action/zipball/e46ed2cbd01164d986452f91f178727624ae40d7'
+curl -s -S -L -o '68bde559dea0fdcac2102bfdf6230c5f70eb485e.tar.gz' 'https://api.github.com/repos/github/codeql-action/tarball/68bde559dea0fdcac2102bfdf6230c5f70eb485e'
+curl -s -S -L -o '68bde559dea0fdcac2102bfdf6230c5f70eb485e.zip' 'https://api.github.com/repos/github/codeql-action/zipball/68bde559dea0fdcac2102bfdf6230c5f70eb485e'
+popd
